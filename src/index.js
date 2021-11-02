6172bc2a1cb95fa9819d88ed25c7dbaf9148e588
Bryan <bryan.zamora@goitsa.me>
2021-11-02 10:19:09 -0500

    Se agrega api de autenticacion del usuario

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import HomeR from './page/home/HomeR';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
